6c4f5a834494b4fd660c3503ffb5e73365203aa6
jhernande7 <142449723+jhernande7@users.noreply.github.com>
2024-12-11 12:41:19 -0500

    Removed CSC340Project-master folders

--- CSC340Project-master/src/main/java/com/_Project/carServiceApp/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com._Project.carServiceApp.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    //for custom made methods
-}
